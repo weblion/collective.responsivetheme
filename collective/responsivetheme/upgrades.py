d5be5ac185f4cb547faa4b2a7d69d08deb118df8
kysr <jochen.dekeyser@gmail.com>
2013-07-02 14:12:35 +0200

    Only install dependency jarn.jsi18n if not already installed

--- collective/responsivetheme/upgrades.py
+++ collective/responsivetheme/upgrades.py
@@ -1,4 +1,8 @@
+from Products.CMFCore.utils import getToolByName
 
 def v1000_to_v1001(portal_setup):
-    return portal_setup.runAllImportStepsFromProfile('profile-jarn.jsi18n:default')
+    portal_quickinstaller = getToolByName(portal_setup, 'portal_quickinstaller')
+    if not portal_quickinstaller.isProductInstalled('jarn.jsi18n'):
+        return portal_setup.runAllImportStepsFromProfile('profile-jarn.jsi18n:default')
+
 
